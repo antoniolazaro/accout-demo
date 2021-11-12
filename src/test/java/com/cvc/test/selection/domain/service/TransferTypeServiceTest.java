bb97c87c70f11b2a7187fd5aba8e96d897926821
Antonio Lazaro Carvalho Borges <antonio.lazaro@gmail.com>
2021-11-12 20:06:01 -0300

    Implementacao testes tax service

---
+++ src/test/java/com/cvc/test/selection/domain/service/TransferTypeServiceTest.java
@@ -0,0 +1,47 @@
+package com.cvc.test.selection.domain.service;
+
+import com.cvc.test.selection.domain.entity.TransferType;
+import com.cvc.test.selection.domain.exceptions.DateIntervalInvalidException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+public class TransferTypeServiceTest {
+
+    private TransferTypeService transferTypeService;
+
+    @BeforeEach
+    void setUp() {
+        transferTypeService = new TransferTypeService();
+    }
+
+    @Test
+    public void defineTaxDateIntervalInvalidException() {
+        Assertions.assertThrows(DateIntervalInvalidException.class, () -> transferTypeService.defineTax(LocalDate.now().plusDays(-1)));
+    }
+
+    @Test
+    public void defineTaxTypeA() {
+        Assertions.assertEquals(TransferType.A,transferTypeService.defineTax(LocalDate.now()));
+    }
+
+    @Test
+    public void defineTaxTypeB() {
+        Assertions.assertEquals(TransferType.B,transferTypeService.defineTax(LocalDate.now().plusDays(1)));
+    }
+
+    @Test
+    public void defineTaxTypeBLimit10() {
+        Assertions.assertEquals(TransferType.B,transferTypeService.defineTax(LocalDate.now().plusDays(10)));
+    }
+
+    @Test
+    public void defineTaxTypeC() {
+        Assertions.assertEquals(TransferType.C,transferTypeService.defineTax(LocalDate.now().plusDays(11)));
+    }
+}
