f8314b0b5ebf6ac60e423307eb0c3e318dcba8d5
Antonio Lazaro Carvalho Borges <antonio.lazaro@gmail.com>
2021-11-12 18:21:53 -0300

    removing not used class

--- src/main/java/com/cvc/test/selection/domain/repository/TaxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cvc.test.selection.domain.repository;
-
-import com.cvc.test.selection.domain.entity.Tax;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaxRepository extends JpaRepository<Tax, Long> {
-}
