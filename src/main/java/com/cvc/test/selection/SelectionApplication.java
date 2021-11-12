38e34a8e2e48ccd55d2c321e9588444233a42dad
Antonio Lazaro Carvalho Borges <antonio.lazaro@gmail.com>
2021-11-12 15:27:57 -0300

    commit inicial projeto com gitignore

---
+++ src/main/java/com/cvc/test/selection/SelectionApplication.java
@@ -0,0 +1,13 @@
+package com.cvc.test.selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvc/test/selection/SelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvc.test.selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
